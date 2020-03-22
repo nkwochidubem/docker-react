f08b0052b817adb71637a28073a7d792880ecfef
Nkwo Chidubem Ambrose <nkwochidubem@Nkwos-MacBook-Pro.local>
2020-03-22 11:17:54 +0100

    added another test case

--- src/App.test.js
+++ src/App.test.js
@@ -14,4 +14,10 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
 
